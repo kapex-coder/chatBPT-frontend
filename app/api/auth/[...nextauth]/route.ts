f1dbb61a4a3daf54297f6484710c6164c7dff111
kapex-coder <kapex.coder@gmail.com>
2025-01-31 16:21:51 +0530

    Updated: authOptions

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,18 +1,5 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-
-export const authOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-  pages: {
-    signIn: "/login",
-  },
-};
+import { authOptions } from "@/app/lib/utils";
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,6 +1,20 @@
 import { ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import GoogleProvider from "next-auth/providers/google";
 
 export function cn(...classes: ClassValue[]) {
   return twMerge(clsx(classes));
 }
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/login",
+  },
+};
